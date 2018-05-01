01f60d4c07019f3b7d1c00ee42a9ea8f795ca486
Victoria Meng <nights1ststar@protonmail.com>
2018-05-01 19:54:36 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,11 +7,11 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-  "SELECT projects.title, SUM(pledges.amount)
+  "SELECT title, SUM(amount)
   FROM projects
   INNER JOIN pledges ON projects.id = pledges.project_id
-  GROUP BY projects.title
-  ORDER BY projects.title
+  GROUP BY title
+  ORDER BY title
   "
 end
 
